ab55c1ad3fd9ec39a30203db9830b3dd23b7ff8f
MajorOcelot <majorocelot6@gmail.com>
2026-01-26 22:44:23 -0600

    created BaseFrame class to initiate the Frame in Main

---
+++ src/tablehand/BaseFrame.java
@@ -0,0 +1,24 @@
+package tablehand;
+
+import javax.swing.JFrame;
+
+public class BaseFrame extends JFrame 
+{
+	BaseFrame()
+	{
+		// Sets title for frame
+		this.setTitle("Tablehand");
+		
+		// Sets default operation for close button to exit application
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Prevents window from being resized
+		this.setResizable(false);
+		
+		// Sets the size of the window
+		this.setSize(800, 800);
+		
+		// Displays the Frame
+		this.setVisible(true);
+	}
+}
